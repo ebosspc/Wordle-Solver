8a6f05f5edcb43e4954a728a254daacc8b5e4ddb
Ethan Francolla <e.francolla@gmail.com>
2022-01-24 16:39:44 +0000

    added welcome.py

--- src/main.py
+++ src/main.py
@@ -8,8 +8,14 @@ import random as rand
 # Import module that generates the word list 
 import word_list_generation as word_list_generation
 
+# Import a module to welcome the user
+import welcome as welcome
+
 #####-Setup-#####
 # Generate a list of all 5 letter words in English
 word_list_generation.get_5_letter_words()
 from word_list_generation import five_letter_word_list
 print(len(five_letter_word_list))
+
+#####-User Interface-#####
+
---
+++ src/welcome.py
@@ -0,0 +1,4 @@
+# Define a function to print instructions for the game
+def print_instructions():
+    print("Welcome to the Wordle solver tool.")
+    print("This program ")
