928f2d09e6577633c0413af944d894e287eafa95
Ethan-Francolla <e.francolla@gmail.com>
2022-01-24 21:01:06 -0500

    fixed typo

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ welcome.print_instructions()
 
 # Get the resulting data of what 
 word_1_char_1_data = str(input("A: "))
-word_1_char_2_data = str(input("A: "))
-word_1_char_3_data = str(input("A: "))
-word_1_char_4_data = str(input("A: "))
-word_1_char_5_data = str(input("A: "))
+word_1_char_2_data = str(input("D: "))
+word_1_char_3_data = str(input("I: "))
+word_1_char_4_data = str(input("E: "))
+word_1_char_5_data = str(input("U: "))
