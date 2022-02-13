92367bfb23ac00ab0d684ad871dbdb29f9d3854d
Ethan Francolla <e.francolla@gmail.com>
2022-02-13 16:07:52 -0500

    added word list generation comprised of possible wordle inputs

--- src/word_list_generation.py
+++ src/word_list_generation.py
@@ -1,12 +1,26 @@
-# Define a function to generate a list of 5 letter words
-def get_5_letter_words_list():
+# Define a function to generate a list of 5 letter words using every word in the English language
+def get_all_words_5_letter_words_list():
     # Create a list to store 5 letter words
-    global five_letter_words_list
-    five_letter_words_list = []
+    global five_letter_words_list_all_words
+    five_letter_words_list_all_words = []
 
     # Open the word database and add 5 letter words to a corresponding list
     words_database_file = open("Words_English.txt", 'r')
     for line in words_database_file:
         individual_word = line.strip() # Remove the endline character from each line before storing
         if len(individual_word) == 5:
-            five_letter_words_list.append(individual_word) # Break up word into list of characters
+            five_letter_words_list_all_words.append(individual_word) # Add 5 letter word to the list
+
+
+# Define a function to generate a list of 5 letter words using the database of possible word inputs to wordle
+def get_wordle_inputs_5_letter_word_list():
+    # Create a list to store 5 letter words
+    global five_letter_words_list_all_inputs
+    five_letter_words_list_all_inputs = []
+
+    # Open the word database and add 5 letter words to a corresponding list
+    words_database_file = open("Words_Possible-Answers.txt", 'r')
+    for line in words_database_file:
+        individual_word = line.strip() # Remove the endline character from each line before storing
+        if len(individual_word) == 5:
+            five_letter_words_list_all_inputs.append(individual_word) # Add 5 letter word to the list
